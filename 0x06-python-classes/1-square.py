2fcd051152657fdaf18ef7e94d8fd97dffb2652f
Codart-7 <iconmoa@gmail.com>
2022-06-22 08:15:58 +0000

    second

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class that defines a square"""
+
+
+class Square:
+    """class Square defines a square"""
+    def __init___(self, size):
+        """initialize square
+        Args: size (int): size of the square
+        """
+        self.___size = size  #: size of the square
