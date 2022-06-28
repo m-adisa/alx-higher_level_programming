da24833eb9c8098f997720340c562b062175876c
Codart-7 <iconmoa@gmail.com>
2022-06-28 22:43:32 +0000

    done

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -28,4 +28,3 @@ def print_square(size):
 
     for i in range(size):
         print("#" * size)
-        
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -34,4 +34,3 @@ def text_indentation(text):
             s = i + d if s is "" else s + "\n\n" + i + d
 
     print(s[:-3], end="")
-    
