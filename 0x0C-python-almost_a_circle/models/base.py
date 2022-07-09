69d09eab712071d3b53e040b8689dde3aeac9b10
Codart-7 <iconmoa@gmail.com>
2022-07-09 09:03:38 +0000

    create class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,4 +6,20 @@ build on
 
 
 class Base:
-    """The"""
+    """Class with:
+    Private class attribute: __nbobjects
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Instantiation of the class Base
+        Args:
+            - id: instance id
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
