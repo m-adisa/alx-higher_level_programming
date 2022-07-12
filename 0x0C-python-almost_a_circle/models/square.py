f99e109b90e5c772719c69933b1a78ae5763e2ae
Michael Adisa <57605457+Codart-7@users.noreply.github.com>
2022-07-12 21:41:11 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,51 +1,43 @@
 #!/usr/bin/python3
-"""Module containing the definition of class Square
-class Square inherits from class Rectangle
-"""
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """A square is a special kind of rectangle with
-    equal height and width. This class inherits all methods
-    and attributes from the class Rectangle
-    """
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization of the Square class
-        Args:
-            - size: height and width must be assigned to the value of size
-            - x
-            - y
-        """
+        """Initialize a new Square.
 
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Retrieves the size of the square"""
+        """Get/set the size of the Square."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Sets the size value as the equivalent of
-        height and width
-        """
-
         self.width = value
         self.height = value
 
-    def __str__:
-        "Returns the string representation of the square by its attributes"""
-
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width)
-
     def update(self, *args, **kwargs):
-        """The public method assigns values to the attributes.
-        Uses kwargs if args exists and is not empty.
-        """
+        """Update the Square.
 
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
         if args and len(args) != 0:
             a = 0
             for arg in args:
@@ -77,11 +69,15 @@ class Square(Rectangle):
                     self.y = v
 
     def to_dictionary(self):
-        """Return the dictionary representation of a Square"""
-
+        """Return the dictionary representation of the Square."""
         return {
-                "id": self.id,
-                "size": self.width,
-                "x": self.x,
-                "y": self
-                }
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
