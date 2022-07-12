3c78c292080e484cdcd5d667eb4b46b485ed2e23
Codart-7 <iconmoa@gmail.com>
2022-07-12 14:22:10 +0000

    features

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -75,3 +75,13 @@ class Square(Rectangle):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Square"""
+
+        return {
+                "id": self.id,
+                "size": self.width,
+                "x": self.x,
+                "y": self
+                }
