e14e931801cc034b972e0eb41c4bdf41e6cfce65
Codart-7 <iconmoa@gmail.com>
2022-09-22 16:32:01 +0100

    [init] v4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display the body after sent a header variable and GET request
-curl -s -X GET -L -H "X-School-User-Id: 98" "$1"
+# Sends a GET request to a given URL with a header and displays the response
+curl -sL -H 'X-School-User-Id: 98' "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
-"""
-    Find a peak in a list of unsorted integers.
-"""
+"""Algorithms for list of integers."""
 
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers."""
-    if len(list_of_integers) == 0:
-        return None
-    elif len(list_of_integers) == 1:
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
         return list_of_integers[0]
-
-    h = len(list_of_integers) - 1
-    l = 0
-    lis = list_of_integers
-    while h > l:
-        half = (h + l) // 2
-        if lis[half] <= lis[half + 1]:
-            l = half + 1
-        elif lis[half] <= lis[half - 1]:
-            h = half - 1
-        elif lis[half] >= lis[half + 1] and lis[half] >= lis[half - 1]:
-            return lis[half]
-    return lis[l]
